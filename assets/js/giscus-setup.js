19157c84e4469a5c720ecbbadf90a8b1b6e249e0
kiriakifragkia <60307362+kiriakifragkia@users.noreply.github.com>
2025-10-25 22:43:49 +0000

    Deploying to gh-pages from @ kiriakifragkia/kiriakifragkia.github.io@30701d0a73c78b9c7ceed771aa987843fa627c28 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "korinnafrg/kiriakifragkia.github.io",
+    "data-repo": "kiriakifragkia/kiriakifragkia.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
